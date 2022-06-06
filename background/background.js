6d68274ae00164be7f0a0e34ecc204fe5b14ba0a
Иван Лещинский <falenangellll@yandex.ru>
2022-06-06 17:00:12 +0700

    fix

--- background/background.js
+++ background/background.js
@@ -1,6 +1,13 @@
-importScripts('./Api.js', '../common/messageTypes.js')
+importScripts('./Api.js', '../common/messageTypes.js', '../common/openOptionPage.js')
 
 const Api = new _Api();
+
+function login(login, password) {
+  Api.login({ login: login, password: password })
+    .then((...x) => {
+      console.log(x)
+    })
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   switch (true) {
@@ -8,14 +15,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       const url = request.data.serverUrl;
       const login = request.data.login;
       const password = request.data.password;
-
-      console.log(url, login, password)
-
-      Api.login({ login: login, password: password })
-        .then((...x) => {
-          console.log(x)
-        })
+      // ToDo: сделать прокидывание адреса сервера в Api при запуске расширения
+      login(login, password)
       break;
     }
   }
+})
+
+chrome.webNavigation.onCompleted.addListener(
+  function (tabId, changeInfo, tab) {
+    console.log(tabId)
+    console.log(changeInfo)
+    console.log(tab)
+    if (changeInfo.url) {
+      // url has changed; do something here
+
+    }
+  }
+);
+
+chrome.runtime.onInstalled.addListener(function () {
+  if (details.reason !== "update") return;
+
+  openOptionPage();
 })
--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,5 @@
-function openOptionPage() {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL('/html/option.html'));
-  }
-};
+importScripts('../common/openOptionPage.js')
 
 document.querySelector('#options').addEventListener('click', function () {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL('/html/option.html'));
-  }
+  openOptionPage()
 });
