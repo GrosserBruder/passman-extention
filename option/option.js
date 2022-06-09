b84f7db8ac7791daef05b8f5129d70353b519466
Иван Лещинский <falenangellll@yandex.ru>
2022-06-09 11:29:02 +0700

    fix

--- option/option.js
+++ option/option.js
@@ -1,7 +1,26 @@
-function save_options() {
-  var serverUrl = document.getElementById('serverUrl').value;
-  var login = document.getElementById('login').value;
-  var password = document.getElementById('password').value;
+async function successAutorization() {
+  const success = document.querySelector('.authorize.authorize--success')
+  const error = document.querySelector('.authorize.authorize--error')
+
+  error.style.display = "none";
+  success.style.display = "block";
+}
+
+async function errorAutorization() {
+  const success = document.querySelector('.authorize.authorize--success')
+  const error = document.querySelector('.authorize.authorize--error')
+
+  error.style.display = "block";
+  success.style.display = "none";
+}
+
+function save_options(event) {
+  event.preventDefault();
+  const values = new FormData(event.target)
+
+  var serverUrl = values.get('serverUrl');
+  var login = values.get('login');
+  var password = values.get('password');
 
   chrome.runtime.sendMessage({
     type: SAVE_OPTION,
@@ -10,22 +29,22 @@ function save_options() {
     }
   }, function (response) {
     if (response.result === "success") {
-      console.log("success")
+      successAutorization()
     }
     if (response.result === "error") {
-      console.log("error")
+      errorAutorization()
     }
   })
 }
 
 function initial() {
   chrome.storage.local.get('serverUrl', function (x) {
-    document.getElementById('serverUrl').value = x.serverUrl;
+    document.querySelector("input[name='serverUrl']").value = x.serverUrl;
   })
   document.querySelector("input[name='login']").value = 'Grosser';
   document.querySelector("input[name='password']").value = '123Parol123';
 }
 
-document.getElementById('save').addEventListener('click', save_options);
+document.querySelector('form.option_form').addEventListener('submit', save_options);
 
 window.addEventListener('load', initial)
